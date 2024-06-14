28d49078f5349e7ef45405cb413baa1fe83e2bb0
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-06-14 00:40:50 -0500

    Constraints and Tables done

--- tables.sql
+++ tables.sql
@@ -1,4 +1,5 @@
-CREATE TABLE InformacionInstitucion (
+CREATE TABLE InformacionInstitucion
+(
     ruc            CHAR(11) PRIMARY KEY,
     descripcion    VARCHAR(1000) NOT NULL,
     fundador       VARCHAR(100)  NOT NULL,
@@ -7,7 +8,20 @@ CREATE TABLE InformacionInstitucion (
     nombre         VARCHAR(150)  NOT NULL
 );
 
-CREATE TABLE Persona (
+CREATE TABLE Grado
+(
+    id     SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Curso
+(
+    id     SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Persona
+(
     dni             CHAR(8) PRIMARY KEY,
     nombres         VARCHAR(100) NOT NULL,
     primerApellido  VARCHAR(50)  NOT NULL,
@@ -17,7 +31,66 @@ CREATE TABLE Persona (
     email           VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Alumno (
+CREATE TABLE Colaborador
+(
+    dni                   CHAR(8) PRIMARY KEY REFERENCES Persona (dni),
+    sueldoMensual         FLOAT       NOT NULL,
+    cci                   CHAR(20)    NOT NULL,
+    numeroCelular         VARCHAR(15) NOT NULL,
+    horasSemanalesTrabajo INT         NOT NULL
+);
+
+CREATE TABLE Sede
+(
+    id                 SERIAL PRIMARY KEY,
+    coordenadaLongitud DOUBLE PRECISION                     NOT NULL,
+    coordenadaLatitud  DOUBLE PRECISION                     NOT NULL,
+    direccion          VARCHAR(255)                         NOT NULL,
+    construccionFecha  DATE                                 NOT NULL,
+    directorDni        CHAR(8) REFERENCES Colaborador (dni) NOT NULL
+);
+
+CREATE TABLE Consejero
+(
+    dni    CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni),
+    sedeId INT REFERENCES Sede (id) NOT NULL
+);
+
+CREATE TABLE Secretario
+(
+    dni    CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni),
+    sedeId INT REFERENCES Sede (id) NOT NULL
+);
+
+CREATE TABLE Tutor
+(
+    dni    CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni),
+    sedeId INT REFERENCES Sede (id) NOT NULL
+);
+
+CREATE TABLE Profesor
+(
+    dni CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni)
+);
+
+CREATE TABLE Salon
+(
+    aforo         INT,
+    nombreSeccion VARCHAR(50) UNIQUE             NOT NULL,
+    gradoId       INT REFERENCES Grado (id)      NOT NULL,
+    sedeId        INT REFERENCES Sede (id)       NOT NULL,
+    tutorDni      CHAR(8) REFERENCES Tutor (dni) NOT NULL,
+    PRIMARY KEY (nombreSeccion, sedeId)
+);
+
+CREATE TABLE Apoderado
+(
+    dni           CHAR(8) PRIMARY KEY REFERENCES Persona (dni),
+    numeroCelular VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE Alumno
+(
     dni           CHAR(8) PRIMARY KEY REFERENCES Persona (dni),
     nombreSeccion VARCHAR(50) REFERENCES Salon (nombreSeccion) NOT NULL,
     gradoId       INT REFERENCES Grado (id)                    NOT NULL,
@@ -25,72 +98,15 @@ CREATE TABLE Alumno (
     apoderadoDni  CHAR(8) REFERENCES Apoderado (dni)           NOT NULL
 );
 
-CREATE TABLE Apoderado (
-    dni           CHAR(8) PRIMARY KEY REFERENCES Persona (dni),
-    numeroCelular VARCHAR(15) NOT NULL
-);
 
-CREATE TABLE Colaborador (
-    dni                   CHAR(8) PRIMARY KEY REFERENCES Persona (dni),
-    sueldoMensual         FLOAT       NOT NULL,
-    cci                   VARCHAR(20) NOT NULL,
-    numeroCelular         VARCHAR(15) NOT NULL,
-    horasSemanalesTrabajo INT         NOT NULL
-);
-
-CREATE TABLE Consejero (
-    dni    CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni),
-    sedeId INT REFERENCES Sede (id) NOT NULL
-);
-
-CREATE TABLE Secretario (
-    dni    CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni),
-    sedeId INT REFERENCES Sede (id) NOT NULL
-);
-
-CREATE TABLE Tutor (
-    dni    CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni),
-    sedeId INT REFERENCES Sede (id) NOT NULL
-);
-
-CREATE TABLE Profesor (
-    dni CHAR(8) PRIMARY KEY REFERENCES Colaborador (dni)
-);
-
-CREATE TABLE Grado (
-    id     SERIAL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Curso (
-    id     SERIAL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Salon (
-    aforo         INT,
-    nombreSeccion VARCHAR(50)                    NOT NULL,
-    gradoId       INT REFERENCES Grado (id)      NOT NULL,
-    sedeId        INT REFERENCES Sede (id)       NOT NULL,
-    tutorDni      CHAR(8) REFERENCES Tutor (dni) NOT NULL,
-    PRIMARY KEY (nombreSeccion, sedeId)
-);
-
-CREATE TABLE Sede (
-    id                 SERIAL PRIMARY KEY,
-    coordenadaLongitud DOUBLE PRECISION                 NOT NULL,
-    coordenadaLatitud  DOUBLE PRECISION                 NOT NULL,
-    direccion          VARCHAR(255)                     NOT NULL,
-    construccionFecha  DATE                             NOT NULL,
-    directorDni        CHAR(8) REFERENCES Persona (dni) NOT NULL
-);
-
-CREATE TABLE ProfesorSede (
+CREATE TABLE ProfesorSede
+(
     profesorDni CHAR(8) PRIMARY KEY REFERENCES Profesor (dni),
     sedeId      INT REFERENCES Sede (id) NOT NULL
 );
 
-CREATE TABLE ProfesorCursoGrado (
+CREATE TABLE ProfesorCursoGrado
+(
     cursoId          INT REFERENCES Curso (id),
     gradoId          INT REFERENCES Grado (id),
     profesorDni      CHAR(8) REFERENCES Profesor (dni),
@@ -98,8 +114,9 @@ CREATE TABLE ProfesorCursoGrado (
     PRIMARY KEY (cursoId, gradoId, profesorDni)
 );
 
-CREATE TABLE Matricula (
-    year          DATE,
+CREATE TABLE Matricula
+(
+    year          INT,
     alumnoDni     CHAR(8) REFERENCES Alumno (dni),
     sedeId        INT REFERENCES Sede (id),
     gradoId       INT REFERENCES Grado (id)           NOT NULL,
