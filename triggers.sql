60cb6436333871faf632c416d15f22a669938a22
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-06-21 02:52:12 -0500

    Triggers Done

--- triggers.sql
+++ triggers.sql
@@ -40,48 +40,85 @@ EXECUTE FUNCTION check_sueldo_mensual();
 
 ----
 
-CREATE OR REPLACE FUNCTION check_colaborador_roles()
-    RETURNS TRIGGER AS
-$$
-DECLARE
-    existe BOOLEAN;
+CREATE OR REPLACE FUNCTION evitar_solapamiento_colaboradores()
+    RETURNS TRIGGER AS $$
 BEGIN
-    SELECT TRUE INTO existe FROM Consejero WHERE dni = NEW.dni AND sedeId = NEW.sedeId;
-    IF existe THEN
-        RAISE EXCEPTION 'El colaborador ya es consejero en esta sede.';
-    END IF;
-
-    SELECT TRUE INTO existe FROM Secretario WHERE dni = NEW.dni AND sedeId = NEW.sedeId;
-    IF existe THEN
-        RAISE EXCEPTION 'El colaborador ya es secretario en esta sede.';
-    END IF;
-
-    SELECT TRUE INTO existe FROM Tutor WHERE dni = NEW.dni AND sedeId = NEW.sedeId;
-    IF existe THEN
-        RAISE EXCEPTION 'El colaborador ya es tutor en esta sede.';
+    IF (EXISTS (SELECT 1 FROM Profesor WHERE dni = NEW.dni)) OR
+       (EXISTS (SELECT 1 FROM Consejero WHERE dni = NEW.dni)) OR
+       (EXISTS (SELECT 1 FROM Secretario WHERE dni = NEW.dni)) OR
+       (EXISTS (SELECT 1 FROM Director WHERE dni = NEW.dni)) OR
+       (EXISTS (SELECT 1 FROM Tutor WHERE dni = NEW.dni)) THEN
+        RAISE EXCEPTION 'El colaborador con DNI % ya existe en otra tabla hija', NEW.dni;
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER check_consejero_roles_trigger
-    BEFORE INSERT
-    ON Consejero
+CREATE TRIGGER evitar_solapamiento_profesor
+    BEFORE INSERT ON Profesor
     FOR EACH ROW
-EXECUTE FUNCTION check_colaborador_roles();
+EXECUTE FUNCTION evitar_solapamiento_colaboradores();
 
-CREATE TRIGGER check_secretario_roles_trigger
-    BEFORE INSERT
-    ON Secretario
+CREATE TRIGGER evitar_solapamiento_consejero
+    BEFORE INSERT ON Consejero
     FOR EACH ROW
-EXECUTE FUNCTION check_colaborador_roles();
+EXECUTE FUNCTION evitar_solapamiento_colaboradores();
 
-CREATE TRIGGER check_tutor_roles_trigger
-    BEFORE INSERT
-    ON Tutor
+CREATE TRIGGER evitar_solapamiento_secretario
+    BEFORE INSERT ON Secretario
     FOR EACH ROW
-EXECUTE FUNCTION check_colaborador_roles();
+EXECUTE FUNCTION evitar_solapamiento_colaboradores();
+
+CREATE TRIGGER evitar_solapamiento_director
+    BEFORE INSERT ON Director
+    FOR EACH ROW
+EXECUTE FUNCTION evitar_solapamiento_colaboradores();
+
+CREATE TRIGGER evitar_solapamiento_tutor
+    BEFORE INSERT ON Tutor
+    FOR EACH ROW
+EXECUTE FUNCTION evitar_solapamiento_colaboradores();
+
+----
+
+CREATE OR REPLACE FUNCTION evitar_solapamiento_con_alumno()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM Alumno WHERE dni = NEW.dni) THEN
+        RAISE EXCEPTION 'La persona con DNI % ya existe en la tabla Alumno', NEW.dni;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER evitar_solapamiento_colaborador
+    BEFORE INSERT ON Colaborador
+    FOR EACH ROW
+EXECUTE FUNCTION evitar_solapamiento_con_alumno();
+
+CREATE TRIGGER evitar_solapamiento_apoderado
+    BEFORE INSERT ON Apoderado
+    FOR EACH ROW
+EXECUTE FUNCTION evitar_solapamiento_con_alumno();
+
+CREATE OR REPLACE FUNCTION evitar_solapamiento_con_colaborador_apoderado()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM Colaborador WHERE dni = NEW.dni) OR
+       EXISTS (SELECT 1 FROM Apoderado WHERE dni = NEW.dni) THEN
+        RAISE EXCEPTION 'La persona con DNI % ya existe en las tablas Colaborador o Apoderado', NEW.dni;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER evitar_solapamiento_alumno
+    BEFORE INSERT ON Alumno
+    FOR EACH ROW
+EXECUTE FUNCTION evitar_solapamiento_con_colaborador_apoderado();
 
 ----
 
@@ -139,44 +176,6 @@ EXECUTE FUNCTION check_construction_date();
 
 ----
 
-CREATE OR REPLACE FUNCTION enforce_single_active_director()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    IF (SELECT COUNT(*)
-        FROM Director d
-                 JOIN Colaborador c ON d.dni = c.dni
-        WHERE d.sedeId = NEW.sedeId
-          AND c.estaActivo) > 0 THEN
-        RAISE EXCEPTION 'Ya existe un director activo para esta sede.';
-    END IF;
-
-    IF (SELECT COUNT(*)
-        FROM Director d
-        WHERE d.dni = NEW.dni
-          AND d.sedeId <> NEW.sedeId) > 0 THEN
-        UPDATE Colaborador
-        SET estaActivo = FALSE
-        WHERE dni = NEW.dni
-          AND estaActivo = TRUE;
-    END IF;
-
-    UPDATE Colaborador
-    SET estaActivo = TRUE
-    WHERE dni = NEW.dni;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER enforce_single_active_director_trigger
-    BEFORE INSERT OR UPDATE
-    ON Director
-    FOR EACH ROW
-EXECUTE FUNCTION enforce_single_active_director();
-
-----
-
 CREATE OR REPLACE FUNCTION check_max_salones_per_grado()
     RETURNS TRIGGER AS
 $$
@@ -197,41 +196,136 @@ EXECUTE FUNCTION check_max_salones_per_grado();
 
 ----
 
-----
-
-CREATE OR REPLACE FUNCTION enforce_single_active_tutor()
-    RETURNS TRIGGER AS
-$$
+CREATE OR REPLACE FUNCTION validar_aforo_alumno()
+    RETURNS TRIGGER AS $$
+DECLARE
+    aforo_maximo INT;
+    numero_alumnos INT;
 BEGIN
-    IF (SELECT COUNT(*)
-        FROM Tutor t
-                 JOIN Colaborador c ON t.dni = c.dni
-        WHERE t.salonNombreSeccion = NEW.salonNombreSeccion
-          AND t.sedeId = NEW.sedeId
-          AND c.estaActivo) > 0 THEN
-        RAISE EXCEPTION 'Ya existe un tutor activo para este salón en esta sede.';
-    END IF;
+    SELECT aforo INTO aforo_maximo
+    FROM Salon
+    WHERE nombreSeccion = NEW.nombreSeccion AND sedeId = NEW.sedeId;
 
-    IF (SELECT COUNT(*)
-        FROM Tutor t
-        WHERE t.dni = NEW.dni
-          AND (t.salonNombreSeccion <> NEW.salonNombreSeccion OR t.sedeId <> NEW.sedeId)) > 0 THEN
-        UPDATE Colaborador
-        SET estaActivo = FALSE
-        WHERE dni = NEW.dni
-          AND estaActivo = TRUE;
-    END IF;
+    SELECT COUNT(*) + 1 INTO numero_alumnos
+    FROM Alumno
+    WHERE nombreSeccion = NEW.nombreSeccion AND sedeId = NEW.sedeId;
 
-    UPDATE Colaborador
-    SET estaActivo = TRUE
-    WHERE dni = NEW.dni;
+    IF numero_alumnos > aforo_maximo THEN
+        RAISE EXCEPTION 'El aforo del salón ha sido excedido. Aforo máximo: %, Número de alumnos: %', aforo_maximo, numero_alumnos;
+    END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER enforce_single_active_tutor_trigger
-    BEFORE INSERT OR UPDATE
-    ON Tutor
+CREATE TRIGGER trigger_validar_aforo_alumno
+    BEFORE INSERT ON Alumno
     FOR EACH ROW
-EXECUTE FUNCTION enforce_single_active_tutor();
+EXECUTE FUNCTION validar_aforo_alumno();
+
+CREATE OR REPLACE FUNCTION validar_aforo_matricula()
+    RETURNS TRIGGER AS $$
+DECLARE
+    aforo_maximo INT;
+    numero_alumnos INT;
+    nombre_seccion VARCHAR(50);
+    sede_id INT;
+BEGIN
+    SELECT nombreSeccion, sedeId INTO nombre_seccion, sede_id
+    FROM Alumno
+    WHERE dni = NEW.alumnoDni;
+
+    SELECT aforo INTO aforo_maximo
+    FROM Salon
+    WHERE nombreSeccion = nombre_seccion AND sedeId = sede_id;
+
+    SELECT COUNT(*) INTO numero_alumnos
+    FROM Alumno
+    WHERE nombreSeccion = nombre_seccion AND sedeId = sede_id;
+
+    IF numero_alumnos > aforo_maximo THEN
+        RAISE EXCEPTION 'El aforo del salón ha sido excedido. Aforo máximo: %, Número de alumnos: %', aforo_maximo, numero_alumnos + 1;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_validar_aforo_matricula
+    BEFORE INSERT ON Matricula
+    FOR EACH ROW
+EXECUTE FUNCTION validar_aforo_matricula();
+
+----
+
+CREATE OR REPLACE FUNCTION validar_secretario_activo()
+    RETURNS TRIGGER AS $$
+DECLARE
+    esta_activo BOOLEAN;
+    sede_secretario INT;
+BEGIN
+    SELECT estaActivo, sedeId INTO esta_activo, sede_secretario
+    FROM Secretario
+             JOIN Colaborador ON Secretario.dni = Colaborador.dni
+    WHERE Secretario.dni = NEW.secretarioDni;
+
+    IF NOT esta_activo OR sede_secretario != NEW.sedeId THEN
+        RAISE EXCEPTION 'El secretario con DNI % no está activo en la sede %', NEW.secretarioDni, NEW.sedeId;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_validar_secretario_activo
+    BEFORE INSERT ON Matricula
+    FOR EACH ROW
+EXECUTE FUNCTION validar_secretario_activo();
+
+----
+
+CREATE OR REPLACE FUNCTION gestionar_director_reasignacion()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF (SELECT COUNT(*) FROM Director WHERE sedeId = OLD.sedeId AND dni != OLD.dni) = 0 THEN
+        RAISE EXCEPTION 'No se puede reasignar el director sin reemplazo en la sede %', OLD.sedeId;
+    END IF;
+
+    DELETE FROM Director WHERE dni = OLD.dni;
+
+    UPDATE Colaborador SET estaActivo = FALSE WHERE dni = OLD.dni;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_gestionar_director_reasignacion
+    AFTER UPDATE ON Director
+    FOR EACH ROW
+    WHEN (OLD.dni IS DISTINCT FROM NEW.dni)
+EXECUTE FUNCTION gestionar_director_reasignacion();
+
+----
+
+CREATE OR REPLACE FUNCTION gestionar_tutor_reasignacion()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF (SELECT COUNT(*) FROM Tutor WHERE salonNombreSeccion = OLD.salonNombreSeccion AND sedeId = OLD.sedeId AND dni != OLD.dni) = 0 THEN
+        RAISE EXCEPTION 'No se puede reasignar el tutor sin reemplazo en el salón % de la sede %', OLD.salonNombreSeccion, OLD.sedeId;
+    END IF;
+
+    DELETE FROM Tutor WHERE dni = OLD.dni AND salonNombreSeccion = OLD.salonNombreSeccion AND sedeId = OLD.sedeId;
+
+    UPDATE Colaborador SET estaActivo = FALSE WHERE dni = OLD.dni;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_gestionar_tutor_reasignacion
+    AFTER UPDATE ON Tutor
+    FOR EACH ROW
+    WHEN (OLD.dni IS DISTINCT FROM NEW.dni OR OLD.salonNombreSeccion IS DISTINCT FROM NEW.salonNombreSeccion OR OLD.sedeId IS DISTINCT FROM NEW.sedeId)
+EXECUTE FUNCTION gestionar_tutor_reasignacion();
+
+----
