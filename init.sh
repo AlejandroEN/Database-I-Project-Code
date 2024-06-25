1701f545634ebadc6cb3920957a06ebac93d7429
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-06-24 19:58:06 -0500

    Refactor Dockerfile and docker-compose.yml

--- init.sh
+++ init.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
 set -e
-export PGPASSWORD=$POSTGRES_PASSWORD
 
-until psql -h localhost -p 5433 -U $POSTGRES_USER -d $POSTGRES_DB -c '\l'; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f "/sql-files/schemas.sql"
 
-echo "Postgres is up - executing commands"
+schemas=$(psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -t -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE '%datos';")
 
-# psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "/docker-entrypoint-initdb.d/schemas.sql"
-
-# schemas=$(psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE '%Datos';")
-
-# for schema in $schemas
-# do
-#   psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SET search_path TO $schema;"
-
-#   psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "/docker-entrypoint-initdb.d/tables.sql"
-# done
+for schema in $schemas
+  do
+	echo "SET search_path TO $schema;" | cat - /sql-files/tables.sql | psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"
+  done
--- schemas.sql
+++ schemas.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA milDatos;
-CREATE SCHEMA diezMilDatos;
-CREATE SCHEMA cienMilDatos;
-CREATE SCHEMA millonDatos;
+CREATE SCHEMA mil_datos;
+CREATE SCHEMA diez_mil_datos;
+CREATE SCHEMA cien_mil_datos;
+CREATE SCHEMA millon_datos;
