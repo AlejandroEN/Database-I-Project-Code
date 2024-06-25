096a5f49114c1a92b348d4bee965531f4d1846b2
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-06-24 23:01:14 -0500

    Comment out unused code and database configuration updates

--- init.sh
+++ init.sh
@@ -10,8 +10,8 @@ for schema in $schemas; do
 	echo "SET search_path TO $schema;" | cat - /sql-files/tables.sql | psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"
 done
 
-tables=$(psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -t -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'mil_datos' AND table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema');")
+# tables=$(psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -t -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'mil_datos' AND table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema');")
 
-for table in $tables; do
-	psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "COPY mil_datos.$table FROM '/csv-files/$table.csv' DELIMITER ',' CSV HEADER;"
-done
+# for table in $tables; do
+# 	psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "COPY mil_datos.$table FROM '/csv-files/$table.csv' DELIMITER ',' CSV HEADER;"
+# done
